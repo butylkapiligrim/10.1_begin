977698df8d36586160257ccd94d4b1c269e05899
Student Free <butylkapiligrim@gmail.com>
2024-05-14 09:02:46 +0300

    10.1: начало

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 # from src.masks import account_mask, card_mask
+from src.processing import filter_by_state, sort_by_date
 from src.widget import convert_date, number_or_account
 
 #
@@ -24,3 +25,20 @@ inputs = [
 
 for input_str in inputs:
     print(number_or_account(input_str))
+
+"""Проверка модуля processing"""
+
+data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+""" Фильтрация по статусу 'CANCELED'"""
+canceled_data = filter_by_state(data, state="CANCELED")
+print(canceled_data)
+
+""" Сортировка по дате по возрастанию"""
+sorted_data = sort_by_date(data, order="asc")
+print(sorted_data)
